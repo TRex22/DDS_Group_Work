c459e17b4aa581f81d9d284e89b73be2d22b2a54
Jason Chalom <jasonchalom@yahoo.co.uk>
2013-08-31 23:37:57 +0200

    Added Stack Functions and other shit
    Added Stack Functions, Finished Queues stuff, and put some other random
    stuff.

--- Project 2/source.c
+++ Project 2/source.c
@@ -23,32 +23,40 @@ void queue_enqueue(Queue *q, int x, int y){
 }
 
 void queue_dequeue(Queue *q, int *x, int *y){
-	
+	ll_getFront(q->l, x, y);
 }
 
 int queue_size(Queue *q){
-
+	return ll_size(q->l);
 }
 
 /* ------------- STACK FUNCTIONS ------------------ */
 Stack* stack_init(){
-
+	Stack *s = (Stack*)malloc(sizeof(Stack));
+	s->l=ll_init();
 }
 void stack_free(Stack *s){
-
+	ll_free(s->l);
+	free(s);
 }
 
 void stack_push(Stack *s, int x, int y){
-
+	ll_addFront(s->l, x, y);
 }
+//will add if need be
+//with code provided the only way to peek
+//is to get front then add front
+//void stack_peek(Stack *s, int *x, int *y){	
+//}
 
 void stack_pop(Stack *s, int *x, int *y){
-
+	ll_getFront(s->l, x, y);
 }
 
 int stack_size(Stack *s){
-
+	return ll_size(s->l);
 }
+
 
 /* ------------- SEARCH FUNCTIONS ------------------ */
 Grid* ReadWorld(const char* filename){
@@ -59,4 +67,31 @@ Search* FindPath(Grid* g){
 
 
 }
+
+/* --------------MISC FUNCTIONS--------------------- */
+//these are fuctions I have used in FAC and DDS projects
+//which I have found useful
+int PowerFn (int no, int power)
+{
+	int j, ans;
+	if (power == 0)
+	{
+		ans = 1;
+	}
+	else if (power == 1)
+	{
+		ans = no;
+	}
+	else
+	{
+		ans = no;
+		for (j = 1; j < power; j++)
+		{
+			ans = ans*no;
+		}
+	}
+	return ans;
+}
+
+
 
